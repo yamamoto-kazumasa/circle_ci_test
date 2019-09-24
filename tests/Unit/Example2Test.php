0850b6327794328f2ce5054a65277692e9e93d95
yamamoto.kazumasa <k_yamamoto@wh-plus.com>
2019-09-24 17:59:48 +0900

    add: test

---
+++ tests/Unit/Example2Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Example2Test extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
